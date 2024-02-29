3447121685e0875dda6640d712a676f2f141e153
Frances <135648147+fancypog@users.noreply.github.com>
2024-02-29 21:45:56 +0000

    Add function to render todo list when updated

--- todo-list.js
+++ todo-list.js
@@ -1,5 +1,19 @@
 const todoList = [];
 
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = '<p>${todo}</p>';
+    todoListHTML += html;
+  }
+}
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list')
+  .innerHTML = todoListHTML;
+
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
@@ -9,3 +23,4 @@ function addTodo() {
 
   inputElement.value = '';
 }
+
