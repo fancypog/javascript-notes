cd2239b545833b784ac2b4a008dc01e8d002248b
Frances <135648147+fancypog@users.noreply.github.com>
2024-02-29 21:23:06 +0000

    Add notes for arrays

---
+++ arrays-and-loops.js
@@ -0,0 +1,20 @@
+const myArray = [10, 20, 30];
+console.log(myArray);
+
+console.log(myArray[0]);
+myArray[0] = 99;
+console.log(myArray);
+
+
+
+[1, 'hello', true, {name: 'socks'}, [1,2]]
+
+Array.isArray([1,2]);
+typeof [1,2]  //Object. Array is a special object
+
+myArray.push(100);
+console.log(myArray);
+
+myArray.splice(0, 2);
+console.log(myArray);
+
