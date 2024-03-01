acee84a842644016903645e11c3f17ebd856b64e
Frances <135648147+fancypog@users.noreply.github.com>
2024-03-01 13:36:26 +0000

    Add add todo item function

--- todo-list.js
+++ todo-list.js
@@ -1,16 +1,25 @@
 const todoList = [];
 
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+  console.log(name);
+}
+
 function renderTodoList() {
   let todoListHTML = '';
 
   for (let i = 0; i < todoList.length; i++) {
     const todo = todoList[i];
     const html = `
-      <p>${todo} 
-      <button onclick="
-        todoList.splice(${i}, 1);
-        renderTodoList();
-        ">Delete</button>
+      <p>
+        ${todo} 
+        <button onclick="
+          todoList.splice(${i}, 1);
+          renderTodoList();
+        ">
+        Delete
+        </button>
       </p>
       `;
     todoListHTML += html;
