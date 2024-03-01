ca97aea3683bd5e5009428d456418913e34ef20b
Frances <135648147+fancypog@users.noreply.github.com>
2024-03-01 13:37:59 +0000

    Update add todo item function

--- todo-list.js
+++ todo-list.js
@@ -3,7 +3,9 @@ const todoList = [];
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
-  console.log(name);
+
+  todoList.push(name);
+  console.log(todoList);
 }
 
 function renderTodoList() {
