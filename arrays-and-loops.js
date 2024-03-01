a0e53429e8d72486b5fed44e1b8f30646faec611
Frances <135648147+fancypog@users.noreply.github.com>
2024-03-01 23:00:58 +0000

    Update notes

--- arrays-and-loops.js
+++ arrays-and-loops.js
@@ -18,3 +18,41 @@ console.log(myArray);
 myArray.splice(0, 2);
 console.log(myArray);
 
+const array1 =[1 ,2 ,3] //arrays are references
+const array2 = array1 //pointing to the same place in memory
+array2.push(4)
+console.log(array1);
+console.log(array2); //the same
+
+const array3 =array1.slice(); //slice copies values
+
+
+//deconstructuring
+
+const array4 = [1, 2, 3];
+const [firstValue, secondValue] = [1, 2, 3]; //get the first and the second value and save to variables
+
+for (let i = 1; i <= 10; i++) {
+  if(i === 3) {
+    continue; //skip the rest of the iteration 
+  }
+  console.log(i);
+  if(i === 8) {
+    break; //stop the loop early
+  }
+}
+
+
+let i = 1;
+
+while (i <= 10) {
+  if(i % 3 === 0) {
+    i++;  //remember this in a while loop
+    continue;
+  }
+  console.log(i);
+  i++
+}
+
+
+
